dd2ea635b036f8e04a2fa5d9efcc837c90e1f532
Tatiana Korysheva <korosteleva87@gmail.com>
2023-02-10 22:01:07 +0300

    initional commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int cost = 18189;
+        int calculation = 200;
+        int total = cost / calculation;
+
+        System.out.println(total);
+    }
+}
